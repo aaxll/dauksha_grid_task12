cce1563d8a219dd1630fd8db1e632294fe2a1b38
aaxll <37516109+aaxll@users.noreply.github.com>
2020-02-11 22:14:10 +0200

    Add files via upload

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,50 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+
+import static com.codeborne.selenide.Browsers.CHROME;
+
+public class TestBase {
+
+
+    // URLs
+    String loginPageURL = "http://open-eshop.stqa.ru/oc-panel/auth/login/";
+
+    // Admin credentials
+    String adminEmail = "demo@open-eshop.com";
+    String adminPassword = "demo";
+
+   /* hub: local physical WIN10
+    * java -jar selenium-server.jar -role hub
+    *
+    * node1: virtual UBUNTU
+    * java -jar selenium-server.jar -role webdriver -hub http://192.168.174.1:4444/grid/register -port 5556
+    * node2: virtual WIN7
+    *
+    * java -jar selenium-server.jar -role webdriver -hub http://192.168.174.1:4444/grid/register -port 5557
+    *
+    * */
+
+
+    @Before
+    public void setUp() {
+        Configuration.browser = CHROME;
+        Configuration.headless = false;
+        Configuration.browserSize = "1920x1080";
+        Configuration.fastSetValue = true;
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        // прогон только на определенной платформе
+        Configuration.browserCapabilities.setPlatform(Platform.LINUX);
+    }
+
+    @After
+    public void tearDown() {
+       WebDriverRunner.closeWebDriver();
+    }
+
+
+
+}
